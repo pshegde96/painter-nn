b22bb0cc173f7b648a48ee2263d79136a84a88ab
pshegde96 <parihegde96@gmail.com>
2017-01-22 21:11:53 +0530

    Modified batch selection method

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This is a re-implementation of [Neural Network "paints" and Image](http://cs.stanford.edu/people/karpathy/convnetjs/demo/image_regression.html) in TensorFlow.
+This is an implementation of [Neural Network "paints" and Image](http://cs.stanford.edu/people/karpathy/convnetjs/demo/image_regression.html) in TensorFlow.
 
 The neural network is trained on a single image. It accepts the pixel position as input and trains a neural network to output the RGB value of the pixel. Since this NN tries to learn to paint a single image, overfitting is good!
--- painter.py
+++ painter.py
@@ -2,10 +2,14 @@ import cv2
 import tensorflow as tf
 import numpy as np
 import numpy.random as random
+import os
 
 ''' Load the Image '''
-img = cv2.imread('cat.jpg')
+filename = 'earth.jpg'
+img = cv2.imread(filename)
+img = cv2.resize(img,(225,225))
 y_train = img.reshape(-1,3)
+y_sample = y_train.copy()
 
 
 '''Define the required parameters'''
@@ -40,7 +44,7 @@ b[i+1] = tf.Variable(tf.zeros([layers[i+1]]))
 y_pred = tf.matmul(z[i],W[i+1]) + b[i+1]
 
 #L2 Regression loss
-loss = tf.reduce_mean(tf.nn.l2_loss(y_pred-y_target))
+loss = tf.reduce_mean(tf.square(y_pred-y_target))
 
 optimizer = tf.train.AdamOptimizer()
 #optimizer = tf.train.MomentumOptimizer(learning_rate=0.01,momentum=0.9)
@@ -52,18 +56,29 @@ init = tf.initialize_all_variables()
 x_sample = np.array([[[j,i]for i in range(img.shape[0])]for j in range(img.shape[1])])
 x_sample = x_sample.reshape(-1,2) #Convert it to a column vector so that it can be fed to the NN
 
+x_train = x_sample.copy()
+#Random permute training data
+idx = random.permutation(x_train.shape[0])
+x_train = x_train[idx]
+y_train = y_train[idx]
 
 
 ''' Perform Training'''
 print 'Starting Training:'
 with tf.Session() as sess:
     sess.run(init) #Initialize all the variables(parameters)
-
+    sample_point = 0
+    epoch = 0
     for step in range(TRAIN_STEPS):
-        row_batch = random.choice(img.shape[0],size=BATCH_SIZE)
-        col_batch = random.choice(img.shape[1],size=BATCH_SIZE)
-        x_batch = np.vstack((row_batch,col_batch)).T 
-        y_batch = img[row_batch,col_batch]
+        if sample_point+BATCH_SIZE < y_train.shape[0]:
+            x_batch = x_train[sample_point:sample_point+BATCH_SIZE]
+            y_batch = y_train[sample_point:sample_point+BATCH_SIZE]
+            sample_point += BATCH_SIZE
+        else:
+            x_batch = x_train[BATCH_SIZE:]
+            y_batch = y_train[BATCH_SIZE:]
+            sample_point = 0
+
 
         sess.run(train,feed_dict={x:x_batch,y_target:y_batch})
 
@@ -71,13 +86,15 @@ with tf.Session() as sess:
                losses = sess.run(loss,feed_dict={x:x_batch,y_target:y_batch})
                print 'Step: {} Loss: {}'.format(step,losses)
 
-        #After every 1000 epochs sample an image
-        if step%5000 == 0:
-            y_sample = sess.run(y_pred,feed_dict={x:x_sample,y_target:y_train})
-            image_sample = y_sample.reshape(img.shape[0],img.shape[1],3).astype(np.uint8)
-            #Store the sample
-            cv2.imwrite('samples/sample_epoch'+str(step)+'.jpg',image_sample)
-            #cv2.imshow('sample_image',image_sample)
-            #cv2.waitKey(0)
+        #After 100 every epoch sample an image
+        if sample_point == 0:
+            epoch += 1
+            if epoch%100 == 0:
+                y_sample = sess.run(y_pred,feed_dict={x:x_sample,y_target:y_train})
+                image_sample = y_sample.reshape(img.shape[0],img.shape[1],3).astype(np.uint8)
+                #Store the sample
+                sample_filename = 'samples/sample_'+os.path.splitext(filename)[0]+'_epoch'+str(epoch)+'.jpg'
+                print 'Sample stored in:  '+sample_filename
+                cv2.imwrite(sample_filename,image_sample)
 
 
